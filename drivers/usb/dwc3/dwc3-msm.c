d9152354e45463e50db5ba10c9c2f9a6976406e6
mohamed.khadri <mohamed.khadri@lge.com>
2013-09-25 11:37:37 -0700

    usb: dwc3: reduce the charger detection dcd poll time to 1.5s
    There are some old chargers which have dp/dm floating. Since
    that charger are out of spec, usb driver cannot detect charger
    type until poll retry end. Previously it took 10 sec.
    Reduce the charger detection retry time to 1.5s for quick
    abnormal charger detection.
    
    Change-Id: Idf671f23c2372aeed0b279865fa2c4f2088a8d31
    Signed-off-by: mohamed.khadri <mohamed.khadri@lge.com>
    Signed-off-by: choongryeol.lee <choongryeol.lee@lge.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1769,7 +1769,7 @@ static void dwc3_start_ta_det(void)
 #endif
 
 #define DWC3_CHG_DCD_POLL_TIME		(100 * HZ/1000) /* 100 msec */
-#define DWC3_CHG_DCD_MAX_RETRIES	100 /* Tdcd_tmout = 100 * 100 msec */
+#define DWC3_CHG_DCD_MAX_RETRIES	15 /* Tdcd_tmout = 15 * 100 msec */
 #define DWC3_CHG_PRIMARY_DET_TIME	(50 * HZ/1000) /* TVDPSRC_ON */
 #define DWC3_CHG_SECONDARY_DET_TIME	(50 * HZ/1000) /* TVDMSRC_ON */
 
